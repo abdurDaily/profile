fc2176f49f1a3256fea781440eec292505eede86
abdurDaily <abdurdiary@gmail.com>
2024-04-11 01:19:57 -0700

    js scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,40 @@ let typed = new Typed(".auto_type", {
   backSpeed:100,
   loop:true
 });
+
+// **** PAGE SCROLL PROGRESS
+
+window.addEventListener('scroll', function(){
+  let progress = document.querySelector('#scroll_progress');
+  let progressValue = document.querySelector('.progress_value');
+  let pos =  document.documentElement.scrollTop;
+  let clientHeight =  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrollValue = Math.round(pos * 100/clientHeight);
+
+  if(scrollValue == 0){
+    progress.classList.remove("prgress_active");
+  }else{
+    progress.classList.add("prgress_active");
+    progress.style.background = `conic-gradient(#55e6a5 ${scrollValue}%, black ${scrollValue}%)`;
+    // progressValue.textContent = scrollValue;
+  }
+});
+
+
+// *** CUSTOM MOUSE EVENT
+let cursoreDot =  document.querySelector('.cursor-dot');
+let cursoreOutline =  document.querySelector('.cursor-outline');
+
+window.addEventListener('mousemove', function(e){
+  let posX = e.clientX;
+  let posY = e.clientY;
+
+
+  cursoreDot.style.left =  `${posX}px`;
+  cursoreDot.style.top =  `${posY}px`;
+
+
+  cursoreOutline.style.left =  `${posX}px`;
+  cursoreOutline.style.top =  `${posY}px`;
+
+})
